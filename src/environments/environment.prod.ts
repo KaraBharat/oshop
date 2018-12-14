d9a06c3b97fefc458d47d629dc5623285a0b0f39
Kara Bharat <karabharat@gmail.com>
2018-12-14 23:48:49 +0530

    change key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCpI-56JKb4tOGaRVLqXHcLr7NdWB4tHis',
+    apiKey: 'XXXXXX',
     authDomain: 'oshop-6afff.firebaseapp.com',
     databaseURL: 'https://oshop-6afff.firebaseio.com',
     projectId: 'oshop-6afff',
     storageBucket: 'oshop-6afff.appspot.com',
-    messagingSenderId: '12929843230'
+    messagingSenderId: 'XXXXXX'
   }
 };
